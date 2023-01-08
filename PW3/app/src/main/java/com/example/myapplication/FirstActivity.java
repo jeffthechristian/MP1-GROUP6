d1a25a8cf44b86252523f469e2ae601af6e30250
Emils Klavins <daiiviids@gmail.com>
2023-01-08 21:59:20 +0200

    dropdown

--- PW3/app/src/main/java/com/example/myapplication/FirstActivity.java
+++ PW3/app/src/main/java/com/example/myapplication/FirstActivity.java
@@ -1,25 +1,60 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class FirstActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinner;
+    private static final String[] paths = {"Default", "Light theme", "Dark theme"};
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
         Button btnGoSecond = (Button) findViewById(R.id.buttonGo2);
-
         btnGoSecond.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(FirstActivity.this, SecondActivity.class));
             }
         });
+
+        spinner = (Spinner)findViewById(R.id.dropdown);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(FirstActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 }
