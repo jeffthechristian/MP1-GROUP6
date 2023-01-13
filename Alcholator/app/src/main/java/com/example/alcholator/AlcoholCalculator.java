709704e52bf7493d63b68b39304b2cd662789a73
Emils Klavins <daiiviids@gmail.com>
2023-01-13 14:07:43 +0200

    back button in alc calc

--- Alcholator/app/src/main/java/com/example/alcholator/AlcoholCalculator.java
+++ Alcholator/app/src/main/java/com/example/alcholator/AlcoholCalculator.java
@@ -2,7 +2,10 @@ package com.example.alcholator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlcoholCalculator extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class AlcoholCalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alcohol_calculator);
+
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AlcoholCalculator.this, DataInput.class));
+            }
+        } );
     }
 }
