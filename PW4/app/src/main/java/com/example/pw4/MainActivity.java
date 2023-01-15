757107a45b71a400b9aeb2235993484a1402e914
Krista <kristabriede01@gmail.com>
2023-01-15 19:25:22 +0200

    bug fix

--- PW4/app/src/main/java/com/example/pw4/MainActivity.java
+++ PW4/app/src/main/java/com/example/pw4/MainActivity.java
@@ -12,48 +12,41 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button0, button1, button2,
-            button3, button4, button5,
-            button6, button7, button8,
-            button9,buttonAdd, buttonSub,
-            buttonMul, buttonDiv, buttonEql,
-            buttonDot, buttonDel, buttonMS,
-            buttonMR, buttonMC;
-    EditText inputText;
     float firstValue, secValue;
     boolean addition, subtract, multiplication, division;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPref =  getSharedPreferences("MyShred", Context.MODE_PRIVATE);
 
-        button0 = (Button) findViewById(R.id.button0);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
-        button5 = (Button) findViewById(R.id.button5);
-        button6 = (Button) findViewById(R.id.button6);
-        button7 = (Button) findViewById(R.id.button7);
-        button8 = (Button) findViewById(R.id.button8);
-        button9 = (Button) findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 =  findViewById(R.id.button3);
+        Button button4 =  findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 =  findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
 
-        buttonAdd = (Button) findViewById(R.id.buttonAdd);
-        buttonSub = (Button) findViewById(R.id.buttonSub);
-        buttonMul = (Button) findViewById(R.id.buttonMul);
-        buttonDiv = (Button) findViewById(R.id.buttonDiv);
-        buttonEql = (Button) findViewById(R.id.buttonEql);
-        buttonDot = (Button) findViewById(R.id.buttonDot);
-        buttonDel = (Button) findViewById(R.id.buttonDel);
+        Button  buttonAdd = findViewById(R.id.buttonAdd);
+        Button buttonSub = findViewById(R.id.buttonSub);
+        Button buttonMul = findViewById(R.id.buttonMul);
+        Button buttonDiv = findViewById(R.id.buttonDiv);
+        Button buttonEql = findViewById(R.id.buttonEql);
+        Button buttonDot = findViewById(R.id.buttonDot);
+        Button buttonDel = findViewById(R.id.buttonDel);
 
-        buttonMS = (Button) findViewById(R.id.buttonMS);
-        buttonMR = (Button) findViewById(R.id.buttonMR);
-        buttonMC = (Button) findViewById(R.id.buttonMC);
+        Button buttonMS = findViewById(R.id.buttonMS);
+        Button buttonMR = findViewById(R.id.buttonMR);
+        Button buttonMC =  findViewById(R.id.buttonMC);
 
-        inputText = (EditText) findViewById(R.id.edt1);
+      EditText inputText = findViewById(R.id.edt1);
 
-
+//kalkulatora pogu nolasisana
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -124,15 +117,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//pogu funkcionalitates izveide, kas atbild par atminas saglabasanu
         buttonMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
                 SharedPreferences.Editor myEdit = sharedPreferences.edit();
-                myEdit.putInt(String.valueOf(inputText), Integer.parseInt(inputText.getText().toString()));
-                myEdit.commit();
-            }
+
+                //ja ir tuksa vertiba ievades lauku atstaj tuksu
+                try {
+                    myEdit.putInt(String.valueOf(inputText), Integer.parseInt(inputText.getText().toString()));
+                    myEdit.commit();
+                }catch (NumberFormatException nfe){
+                    inputText.setText("");
+                }
+                }
+
         });
 
         buttonMC.setOnClickListener(new View.OnClickListener() {
@@ -153,29 +153,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+//matematisko pogu un darbibu definesana
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (inputText == null) {
-                    inputText.setText("");
-                } else {
+
+                try {
                     firstValue = Float.parseFloat(inputText.getText() + "");
                     addition = true;
                     inputText.setText(null);
+                }catch (NumberFormatException nfe){
+                        inputText.setText("");
+                    }
                 }
-            }
+
         });
 
         buttonSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (inputText == null) {
-                    inputText.setText("");
-                } else {
+                try {
                     firstValue = Float.parseFloat(inputText.getText() + "");
                     subtract = true;
                     inputText.setText(null);
+                }catch (NumberFormatException nfe) {
+                    inputText.setText("");
                 }
             }
         });
@@ -183,59 +185,64 @@ public class MainActivity extends AppCompatActivity {
         buttonMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (inputText == null) {
-                    inputText.setText("");
-                } else {
+                 try {
                     firstValue = Float.parseFloat(inputText.getText() + "");
                     multiplication = true;
                     inputText.setText(null);
-                }
+                }catch (NumberFormatException nfe){
+                     inputText.setText("");
+                 }
             }
         });
 
         buttonDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (inputText == null) {
-                    inputText.setText("");
-                } else {
+                try {
                     firstValue = Float.parseFloat(inputText.getText() + "");
                     division = true;
                     inputText.setText(null);
+                }catch (NumberFormatException nfe){
+                    inputText.setText("");
                 }
             }
         });
 
-
+//parbauda kura no matematiskajam darbibam ir patiesa un veic matematisko darbibu
         buttonEql.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    secValue = Float.parseFloat(inputText.getText() + "");
 
-                secValue = Float.parseFloat(inputText.getText() + "");
+                    if (addition == true) {
+                        inputText.setText(firstValue + secValue + "");
+                        addition = false;
+                    }
+                    if (subtract == true) {
+                        inputText.setText(firstValue - secValue + "");
+                        subtract = false;
+                    }
 
-                if (addition == true) {
-                    inputText.setText(firstValue + secValue + "");
-                    addition = false;
-                }
-                if (subtract == true) {
-                    inputText.setText(firstValue - secValue + "");
-                    subtract = false;
-                }
+                    if (multiplication == true) {
+                        inputText.setText(firstValue * secValue + "");
+                        multiplication = false;
+                    }
 
-                if (multiplication == true) {
-                    inputText.setText(firstValue * secValue + "");
-                    multiplication = false;
-                }
-
-                if (division == true) {
-                    inputText.setText(firstValue / secValue + "");
-                    division = false;
+                    if (division == true) {
+                        inputText.setText(firstValue / secValue + "");
+                        division = false;
+                    }
+                } catch (NumberFormatException nfe) {
+                    inputText.setText("");
                 }
             }
 
         });
 
 
+
+// ja nospiez pogu C tiek nodzests ievades lauks
         buttonDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -243,6 +250,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+ // ievades lauka lauj ievadit dalskaitlus ar punktu pa vidu
         buttonDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
