3f3c6b40e9a9f5b2af2e9d088777d4bc76dd04e3
Emils Klavins <daiiviids@gmail.com>
2023-01-15 20:34:31 +0200

    button in bring me back

--- Alcholator/app/src/main/java/com/example/alcholator/CheckboxScreen.java
+++ Alcholator/app/src/main/java/com/example/alcholator/CheckboxScreen.java
@@ -2,7 +2,10 @@ package com.example.alcholator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckboxScreen extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class CheckboxScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkbox_screen);
+
+        Button buttonBring = findViewById(R.id.buttonBring);
+        buttonBring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CheckboxScreen.this, StartActivity.class));
+            }
+        } );
     }
 }
