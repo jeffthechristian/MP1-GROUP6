957fdd7641f634a1ddef92b3068ddd63326115bb
Emils Klavins <daiiviids@gmail.com>
2023-01-15 20:11:51 +0200

    asdffgg

--- Alcholator/app/src/main/java/com/example/alcholator/AlcoholCalculator.java
+++ Alcholator/app/src/main/java/com/example/alcholator/AlcoholCalculator.java
@@ -6,13 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AlcoholCalculator extends AppCompatActivity {
+    private TextView alcStrengthInput, volumeInput;
+    private Button btnSaveData2, btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alcohol_calculator);
+
+        alcStrengthInput =(TextView) findViewById(R.id.alcStrengthInput);
+        volumeInput=(TextView) findViewById(R.id.volumeInput);
+
+        btnSaveData2=(Button) findViewById(R.id.btnSaveData2);
+        btnBack=(Button) findViewById(R.id.btnBack);
+
+        double alcStrength = Integer.parseInt(alcStrengthInput.getText().toString());
+        double volume = Integer.parseInt(volumeInput.getText().toString());
+
+        btnSaveData2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AlcoholCalculator.this, DataInput.class));
+                calculate();
+            }
+        } );
 
         Button btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -22,4 +42,6 @@ public class AlcoholCalculator extends AppCompatActivity {
             }
         } );
     }
+    public void calculate(){
+    };
 }
--- Alcholator/app/src/main/java/com/example/alcholator/DataInput.java
+++ Alcholator/app/src/main/java/com/example/alcholator/DataInput.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class DataInput extends AppCompatActivity {
+    public CheckBox maleBox, femaleBox;
+    public TextView weightInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_input);
+
+        maleBox=(CheckBox) findViewById(R.id.maleBox);
+        femaleBox=(CheckBox) findViewById(R.id.femaleBox);
+
+        weightInput = (TextView) findViewById(R.id.weightInput);
+
+
+        double gender =(double) genderCheck();
+        double weight = Integer.parseInt(weightInput.getText().toString());
+
 
         //Go to alc. calculator activity with data input
         Button btnSaveData = findViewById(R.id.btnSaveData);
@@ -32,4 +46,25 @@ public class DataInput extends AppCompatActivity {
             }
         } );
     }
+    public void saveData(){
+    };
+
+    public void skipData(){
+    };
+
+    public double genderCheck() {
+        double male = 0;
+        double female = 0;
+        double noGender = 0;
+        double gender = 0;
+
+        if(maleBox.isChecked()){
+            gender=male;
+        }else if(femaleBox.isChecked()){
+            gender=female;
+        }else{
+            gender=noGender;
+        };
+        return gender;
+    };
 }
