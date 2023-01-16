a9758e32a8c0f6fd9e2058fcb4acd5e03782879b
jeffthechristian <daiiviids@gmail.com>
2023-01-16 13:04:41 +0200

    added back button in result screen

--- Alcholator/app/src/main/java/com/example/alcholator/ResultActivity.java
+++ Alcholator/app/src/main/java/com/example/alcholator/ResultActivity.java
@@ -3,8 +3,11 @@ package com.example.alcholator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -26,5 +29,13 @@ public class ResultActivity extends AppCompatActivity {
 
         bloodResult.setText(sprom);
         soberResult.setText(ssober);
+
+        Button btnBack2 = findViewById(R.id.btnBack2);
+        btnBack2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ResultActivity.this, AlcoholCalculator.class));
+            }
+        } );
     }
 }
